6820c3c30ffefad96392007efc944bc0995846b4
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-10-05 08:52:47 +0000

    [Create]Active Strage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
   
   config.hosts << "a617d2ebf1dc46c79cc410889baa4f1a.vfs.cloud9.ap-northeast-1.amazonaws.com"
   
+  config.active_job.queue_adapter = :inline
+  
 end
